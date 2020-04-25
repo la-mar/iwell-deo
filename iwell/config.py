74d6068501afe322102618f97841cd7810c09736
Brock Friedrich <brocklfriedrich@gmail.com>
2020-04-25 08:16:07 -0500

    another attempt at wranging the worker memory leak

--- iwell/config.py
+++ iwell/config.py
@@ -155,10 +155,10 @@ class BaseConfig:
     )  # 10 minutes
     CELERY_TASK_SERIALIZER = "pickle"
     CELERY_ACCEPT_CONTENT = ["json", "pickle"]
-    # CELERYD_MAX_TASKS_PER_CHILD = os.getenv("CELERYD_MAX_TASKS_PER_CHILD", 1000)
-    # CELERYD_MAX_MEMORY_PER_CHILD = os.getenv(
-    #     "CELERYD_MAX_MEMORY_PER_CHILD", 200000
-    # )  # 200MB
+    CELERYD_MAX_TASKS_PER_CHILD = int(os.getenv("CELERYD_MAX_TASKS_PER_CHILD", 100))
+    CELERYD_MAX_MEMORY_PER_CHILD = int(
+        os.getenv("CELERYD_MAX_MEMORY_PER_CHILD", 10000)
+    )  # 100MB
     CELERY_ENABLE_REMOTE_CONTROL = False  # required for sqs
     CELERY_SEND_EVENTS = False  # required for sqs
     CELERY_DEFAULT_QUEUE = "iwell-celery"  # sqs queue name
